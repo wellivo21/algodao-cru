b105c534be6b2bea34e79155ef2ff4e9aa8028a1
Wellington Ivo <wellingtonivoprog@gmail.com>
2022-10-04 11:43:55 -0300

    add all JS functionality. Change html and css to match some changes
    Add all JS functionality. Change some tags attributes in the HTML and add a new style for when the submit button is disabled.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,62 @@
-// build a function to put a "*" inside label p if the input has the required attribute
+'use strict';
+// query selectors
+const formEl = document.querySelector('.sub-form');
+const allInputsEl = document.querySelectorAll('input');
+const submitBtnEl = document.querySelector('.submit-btn');
+
+/**
+ * Summary. This function adds a "*" to the <span> of the labels whose input is required (required attr is present).
+ */
+const requiredInput = () => {
+  allInputsEl.forEach((input) => {
+    if (input.hasAttribute('required')) {
+      const spanEl = input.previousElementSibling;
+      spanEl.innerHTML += '*';
+    }
+  });
+};
+
+const formData = () => {
+  const formData = {};
+  formEl.addEventListener('submit', (event) => {
+    event.preventDefault();
+    allInputsEl.forEach((input) => {
+      if (input.hasAttribute('required')) {
+        // getting info from inputs with the required attr
+        formData[input.name] = input.value;
+      }
+      // getting checked-radio-btn data. This part has no meaning at all, I just wanted to to it :)
+      const checkedRadio = document.querySelector(
+        "input[type='radio']:checked"
+      );
+      formData[checkedRadio.name] = checkedRadio.value;
+    });
+    console.log(formData);
+    return formData;
+  });
+};
+
+/**
+ * Description. This function checks if the answer typed in by the user, in the validator input, is correct. If it is, the submit-btn is then clickable. If not, it'll stay disabled.
+ */
+const formValidator = () => {
+  const validadorInput = document.querySelector('input[name="validador"]');
+  const VALIDADOR_NUMERO = '15';
+
+  // initially put the button to be disabled
+  submitBtnEl.setAttribute('disabled', 'true');
+
+  validadorInput.addEventListener('input', () => {
+    validadorInput.value === VALIDADOR_NUMERO
+      ? submitBtnEl.removeAttribute('disabled')
+      : submitBtnEl.setAttribute('disabled', 'true');
+  });
+};
+
+const init = () => {
+  requiredInput();
+  formValidator();
+  formData();
+};
+
+init();
